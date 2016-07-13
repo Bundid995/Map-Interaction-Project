22193da66d6237a77047ed36d68aa8214dca4b76
bundid rakkham <bundid rakkham>
2016-07-13 23:32:31 +0700

    56565656

---
+++ src/main/java/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class HomeController {
+    @RequestMapping("/hello")
+    public ModelAndView helloWorld() {
+        String message = "HELLO SPRING MVC HOW R U";
+        return new ModelAndView("hellopage", "message", message);
+    }
+}
